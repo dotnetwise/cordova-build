d76b2a94de718e3a612bb1e616cabeb553b77b60
Laurenţiu Macovei <laureniu.macovei@dotnetwise.com>
2014-04-24 17:05:11 +0300

    Fail silently if stdin is not available

--- bin/cmd.js
+++ bin/cmd.js
@@ -16,25 +16,29 @@ var listen = conf.listen;
 //        process.exit(err && err.code)
 //    });
 //});
-process.openStdin().on('keypress', function (chunk, key) {
-    if (key && key.name === 'c' && key.ctrl) {
-        process.emit('SIGINT');
-        process.exit();
+    try {
+        process.openStdin().on('keypress', function (chunk, key) {
+            if (key && key.name === 'c' && key.ctrl) {
+                process.emit('SIGINT');
+                process.exit();
+            }
+        });
+
+        if (process.platform === 'win32') {
+            var readLine = require('readline');
+            var rl = readLine.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+
+            rl.on('SIGINT', function () {
+                process.emit('SIGINT');
+            });
+
+        }
     }
-});
-
-if (process.platform === 'win32') {
-    var readLine = require('readline');
-    var rl = readLine.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    rl.on('SIGINT', function () {
-        process.emit('SIGINT');
-    });
-
-}
+    finally {
+    }
 
 if (listen.server || listen.ui) {
     var server = conf.serverInstance = new cordovaBuild.Server(conf);
